36aec7eb0b6f84de01831f0e1bc23d8aa6fb3eae
Jonathan Knapp <jon@coffeeandcode.com>
2025-09-25 09:34:48 -0400

    Try to unpack error response in case it's nil

--- lib/tableau_api/error.rb
+++ lib/tableau_api/error.rb
@@ -2,11 +2,18 @@
 
 module TableauApi
   class TableauError < StandardError
+    MISSING_RESPONSE_ERROR = {
+      'code' => 1_000,
+      'detail' => '',
+      'summary' => 'Tableau API response was missing'
+    }.freeze
+
     attr_reader :http_response_code, :error_code, :summary, :detail
 
     def initialize(net_response)
       @http_response_code = net_response.code
-      error = HTTParty::Parser.new(net_response.body, :xml).parse['tsResponse']['error']
+      ts_error = HTTParty::Parser.new(net_response.body, :xml).parse['tsResponse']
+      error = ts_error.nil? ? MISSING_RESPONSE_ERROR : ts_error['error']
       @error_code = error['code']
       @summary = error['summary']
       @detail = error['detail']
